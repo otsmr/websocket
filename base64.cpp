1db72dbc18bc982a6162758fe2e79fe6e4cd197d
otsmr <git@tsmr.eu>
2022-01-27 03:51:34 +0100

    data type update

--- base64.cpp
+++ base64.cpp
@@ -8,7 +8,7 @@
 
 char characters[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-void base64_decode (char *input, char **output, int *length) {
+void base64_decode (char *input, uint8_t **output, uint64_t *length) {
 
     int len = strlen(input);
 
@@ -16,18 +16,19 @@ void base64_decode (char *input, char **output, int *length) {
         return;
 
     *length = len - len/4;
-    *output = (char *) calloc(*length+1, sizeof(char));
+    *output = (uint8_t *) calloc(*length+1, sizeof(uint8_t));
 
-    char tmp[4];
-    char *out = *output;
+    char i;
+    uint8_t tmp[4];
+    uint8_t *out = *output;
 
     char reverse_characters['z'+1];
-    for (char i = 0; i < 65; i++)
+    for (i = 0; i < 65; i++)
         reverse_characters[characters[i]] = i;
 
     while (*input != '\00')
     {
-        for (char i = 0; i < 4; i++)
+        for (i = 0; i < 4; i++)
         {
             if (*(input+i) == '=') {
                 *(tmp+i) = '\00';
@@ -48,18 +49,20 @@ void base64_decode (char *input, char **output, int *length) {
     
 }
 
-void base64_encode (char *input, char **output, int length) {
+void base64_encode (uint8_t *input, char **output, uint64_t length) {
 
     *output = (char *) calloc(ceil(length * (4/3) + 1), sizeof(char));
 
     char * out = *output;
-    char tmp[3];
-    int pos = 0;
-    
-    while ((length-pos) > 0)
+    char i;
+
+    uint8_t tmp[3];
+    uint64_t pos = 0;
+
+    while (length > pos)
     {
 
-        for (char i = 0; i < 3; i++)
+        for (i = 0; i < 3; i++)
         {
             if ((length-pos-i) <= 0) {
                 *(tmp+i) = '\00';
@@ -68,20 +71,26 @@ void base64_encode (char *input, char **output, int length) {
             }
         }
 
-        *(out+0) = characters[   *tmp >> 2                              ];
-        *(out+1) = characters[ ( *tmp    & 3   ) << 4 | (*(tmp+1) >> 4) ];
-        *(out+2) = characters[ (*(tmp+1) & 15  ) << 2 | (*(tmp+2) >> 6) ];
-        *(out+3) = characters[  *(tmp+2) & 63                           ];
+        /* 
+         * +--first octet--+-second octet--+--third octet--+
+         * |7 6 5 4 3 2 1 0|7 6 5 4 3 2 1 0|7 6 5 4 3 2 1 0|
+         * +-----------+---+-------+-------+---+-----------+
+         * |5 4 3 2 1 0|5 4 3 2 1 0|5 4 3 2 1 0|5 4 3 2 1 0|
+         * +--1.index--+--2.index--+--3.index--+--4.index--+
+         */
+
+        *(out+0) = characters[   *tmp >> 2                                  & 63 ];
+        *(out+1) = characters[ (( *tmp    & 3   ) << 4 | (*(tmp+1) >> 4))   & 63 ];
+        *(out+2) = characters[ ((*(tmp+1) & 15  ) << 2 | (*(tmp+2) >> 6))   & 63 ];
+        *(out+3) = characters[  *(tmp+2)                                    & 63 ];
     
         out += 4;
         pos += 3;
 
     }
 
-    for (char i = 0; i < (3 - length%3)%3; i++)
-    {
+    for (i = 0; i < ( 3 - (length%3) ) % 3; i++)
         *(out-i-1) = '=';
-    }
 
     *out = '\00';
 
--- base64.h
+++ base64.h
@@ -9,5 +9,5 @@
 #include <stdlib.h>
 #include <cmath>
 
-void base64_decode (char *input, char **output, int *output_length);
-void base64_encode (char *input, char **output, int length);
+void base64_decode (char *input, uint8_t **output, uint64_t *length);
+void base64_encode (uint8_t *input, char **output, uint64_t length);
