7c66d662b2295fe45d117d770cc277c4cf4977de
otsmr <git@tsmr.eu>
2022-06-17 17:55:02 +0200

    some

--- src/crypto/curve-x25519.cpp
+++ src/crypto/curve-x25519.cpp
@@ -4,6 +4,8 @@
  */
 
 #include "curve-x25519.h"
+#include "iostream"
+#include "vector"
 
 namespace Crypto {
 
@@ -34,32 +36,58 @@ uint8_t modular_multiply(uint8_t a, uint8_t b, uint8_t p) {
 
 uint8_t modular_add();
 
+uint32_t get_inverse(uint32_t a, uint32_t p) {
 
-uint8_t get_inverse(uint8_t a) {
+    // Fermat's little theorem
+    // r = a^-1 mod p
 
-    uint8_t b;
+    uint32_t r;r
 
 
-    return b;
+
+    return r;
 };
 
 Point point_addition (Point a, Point b) {
 
+    a = {5, 1};
+    b = {5, 1};
+    
+
+    std::cout << "\n\n2P = P + P = (5,1) + (5,1) = (x, y)\n\n" << std::endl;
+
+    
+
     Point c;
 
+    uint32_t x = get_inverse(473, 2413);
+
+    std::cout << "get_inverse(473, 2413)" << x << std::endl;
+
+    return c;
+
     uint8_t s;
-    uint8_t p = 17;
 
-    // s Berechnen
+    // Parameter der elliptischen Kurve
+    const uint8_t E_p = 17U;
+    const uint8_t E_a = 2U;
+
+    // 1. s berechnen
+
     if (a.x == b.x && a.y == b.y) {
-        // a == b
-        // (3x1^2 + a) / 2y1       -> a = 2
+        // wenn es sich um den selben Punkt handelt
+
+        // (3x1^2 + a) / 2y1
 
 
-        uint8_t oben =  modular_multiply(modular_multiply(a.x, a.x), 3) + 2; // modular_multiply
-        uint8_t unten = get_inverse(2 * a.y);            // get_inverse();
+        // uint8_t oben =  modular_multiply(modular_multiply(a.x, a.x, E_p), 3, E_p) + E_a;
+        // std::cout << "oben=" << oben << std::endl;
 
-        s = (oben * unten) % p;
+        uint8_t inverse = get_inverse(2 * a.y, E_p);
+
+        std::cout << "inverse=" << inverse << std::endl;
+
+        // s =  modular_multiply(oben, inverse, E_p);
 
     } else {
 
@@ -68,13 +96,14 @@ Point point_addition (Point a, Point b) {
 
     }
 
-
-    return c;
-
+    std::cout << "s=" << s << std::endl;
+    
     // x3 = s^2 -x1 - x2 mod p
     // y3 = s(x1  x3) - y1 mod p
 
-    // berechnen einer Inversen
+    return c;
+
+
 };
 
 
