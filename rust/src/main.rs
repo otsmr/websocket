0c041b2b5e8acc71944cb272a298b652afa45662
otsmr <git@tsmr.eu>
2023-01-07 23:35:47 +0100

    rust add mutext pointers

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,58 +2,31 @@
 mod websocket;
 use log::info;
 // use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use websocket::WebSocket;
+use websocket::{WebSocket, Message, MessageKind};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
     info!("Starting WebSocket!");
-    let mut ws = WebSocket::new();
 
+    let mut ws = WebSocket::bind(vec!["127.0.0.1:8080".parse().unwrap()]).await?;
+    // info!("Listening on {}", ws.addr.unwrap());
 
-    ws.on_connection(|mut wsc| {
+    ws.on_connection(|wsc| {
         info!("new client connected");
 
-        wsc.on_message(|message| {
-            info!("New Message: {}", message);
+        wsc.on_message(|wsc, message| {
+            info!("New Message: {}", message.string);
+            wsc.send_message(Message {
+                kind: MessageKind::String,
+                string: "Hallo Welt!".to_string()
+            });
         });
 
-        wsc
     });
 
-
-    let wslistener = ws.bind(vec!["127.0.0.1:8080".parse().unwrap()]).await?;
-    info!("Listening on {}", ws.addr.unwrap());
-    wslistener.listen().await;
+    ws.listen().await;
 
     Ok(())
-        // let listener = TcpListener::bind("127.0.0.1:8080").await?;
-
-        // loop {
-        //     info!("Server startet in loop!");
-
-        //     let (mut socket, _) = listener.accept().await?;
-        //     tokio::spawn(async move {
-        //         let mut buf = [0; 1024];
-
-        //         // In a loop, read data from the socket and write the data back.
-        //         loop {
-        //             let n = match socket.read(&mut buf).await {
-    //                 Ok(n) if n == 0 => return,
-    //                 Ok(n) => n,
-    //                 Err(e) => {
-    //                     eprintln!("failed to read from socket; err = {:?}", e);
-    //                     return;
-    //                 }
-    //             };
-
-    //             // Write the data back
-    //             if let Err(e) = socket.write_all(&buf[0..n]).await {
-    //                 eprintln!("failed to write to socket; err = {:?}", e);
-    //                 return;
-    //             }
-    //         }
-    //     });
-    // }
 }
--- rust/src/websocket.rs
+++ rust/src/websocket.rs
@@ -1,23 +1,60 @@
 use log::{error, info};
 use std::io;
 use std::net::SocketAddr;
+use std::sync::Arc;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::Mutex;
 
 #[derive(Debug)]
-pub struct WebSocketClient {
-    socket: TcpStream,
-    on_message_fkt: Option<fn(String) -> ()>,
+pub enum MessageKind {
+    String = 0,
+    // Binary,
 }
 
-impl WebSocketClient {
+#[derive(Debug)]
+pub struct Message {
+    pub kind: MessageKind,
+    pub string: String,
+}
+
+pub struct WebSocketConnection {
+    socket: Arc<Mutex<TcpStream>>,
+    pub on_message_fkt: Option<fn(&mut Self, Message) -> ()>,
+}
+
+impl WebSocketConnection {
+    pub fn on_message(&mut self, f: fn(&mut Self, Message) -> ()) {
+        self.on_message_fkt = Some(f);
+    }
+
+    pub fn send_message(&mut self, msg: Message) {
+        let socket = self.socket.clone();
+        tokio::spawn(async move {
+            let mut socket = socket.lock().await;
+            socket.write(msg.string.as_bytes()).await;
+            // match msg.kind {
+            //     MessageKind::String => {
+            //     }
+            //     // _ => {
+            //     //     error!(
+            //     //         "Transmitter::send MessageKind {:?} not implemented!",
+            //     //         msg.kind
+            //     //     );
+            //     // }
+            // }
+        });
+    }
+
     pub async fn connect(&mut self) -> io::Result<()> {
         info!("Trying to connect with WebSocket");
         let mut buf = [0; 1024];
 
+        let socket = Arc::clone(&self.socket);
         // In a loop, read data from the socket and write the data back.
         loop {
-            let n = match self.socket.read(&mut buf).await {
+            let mut socket = socket.lock().await;
+            let _n = match socket.read(&mut buf).await {
                 Ok(n) if n == 0 => break,
                 Ok(n) => n,
                 Err(e) => {
@@ -26,35 +63,16 @@ impl WebSocketClient {
                 }
             };
 
-            if let Some(fkt) = self.on_message_fkt {
-                fkt(std::str::from_utf8(&buf).unwrap().to_string());
-            }
+            if let Some(on_message) = self.on_message_fkt {
+                let msg = Message {
+                    kind: MessageKind::String,
+                    string: std::str::from_utf8(&buf).unwrap().to_string(),
+                };
 
-            // Write the data back
-            if let Err(e) = self.socket.write_all(&buf[0..n]).await {
-                eprintln!("failed to write to socket; err = {:?}", e);
+                on_message(self, msg);
             }
         }
         Ok(())
-        // // In a loop, read data from the socket and write the data back.
-        // loop {
-
-        //     let _n = match self.socket.read(&mut buf) {
-        //         // socket closed
-        //         Ok(n) if n == 0 => break,
-        //         Ok(n) => n,
-        //         Err(e) => {
-        //             eprintln!("failed to read from socket; err = {:?}", e);
-        //             break;
-        //         }
-        //     };
-
-
-        // }
-        // Ok(())
-    }
-    pub fn on_message(&mut self, f: fn(String) -> ()) {
-        self.on_message_fkt = Some(f);
     }
 }
 
@@ -65,55 +83,13 @@ impl WebSocketClient {
 //     Stopped,
 // }
 
-#[derive(Debug)]
-pub struct WebSocketListener {
-    listener: TcpListener,
-    on_connection_fkt: Option<fn(WebSocketClient) -> WebSocketClient>,
-}
-
-impl WebSocketListener {
-    pub async fn listen(&self) {
-        info!("Waiting for connections!");
-        loop {
-            match self.listener.accept().await {
-                Ok((socket, _addr)) => {
-                    let fkt_opt = self.on_connection_fkt.clone();
-                    tokio::spawn(async move {
-                        let mut sc = WebSocketClient {
-                            socket,
-                            on_message_fkt: None,
-                        };
-                        if let Some(fkt) = fkt_opt {
-                            sc = fkt(sc);
-                        }
-                        sc.connect().await;
-                    });
-                }
-                Err(e) => error!("couldn't get client: {:?}", e),
-            };
-        }
-    }
-}
-
-#[derive(Debug)]
 pub struct WebSocket {
-    pub addr: Option<SocketAddr>,
-    on_connection_fkt: Option<fn(WebSocketClient) -> WebSocketClient>, // clients: Vec<WebSocketClient>,
+    listener: TcpListener,
+    on_connection_fkt: Option<fn(&mut WebSocketConnection) -> ()>,
 }
 
 impl WebSocket {
-    pub fn new() -> Self {
-        WebSocket {
-            addr: None,
-            on_connection_fkt: None, // clients: vec![],
-        }
-    }
-
-    pub fn on_connection(&mut self, f: fn(WebSocketClient) -> WebSocketClient) {
-        self.on_connection_fkt = Some(f);
-    }
-
-    pub async fn bind(&mut self, mut addrs: Vec<SocketAddr>) -> io::Result<WebSocketListener> {
+    pub async fn bind(mut addrs: Vec<SocketAddr>) -> io::Result<Self> {
         let mut listener: Option<TcpListener> = None;
 
         if addrs.is_empty() {
@@ -122,17 +98,15 @@ impl WebSocket {
 
         for addr in addrs {
             if let Ok(ok_listener) = TcpListener::bind(addr).await {
-                self.addr = Some(addr);
                 listener = Some(ok_listener);
                 break;
             }
         }
 
         if let Some(listener) = listener {
-            let on_connection_fkt = self.on_connection_fkt.clone();
-            return Ok(WebSocketListener {
+            return Ok(WebSocket {
                 listener,
-                on_connection_fkt,
+                on_connection_fkt: None,
             });
         }
 
@@ -140,5 +114,29 @@ impl WebSocket {
             io::ErrorKind::InvalidInput,
             "could not resolve to any address",
         ))
+    }
+
+    pub fn on_connection(&mut self, f: fn(&mut WebSocketConnection) -> ()) {
+        self.on_connection_fkt = Some(f);
+    }
+
+    pub async fn listen(&self) {
+        info!("Waiting for connections!");
+        loop {
+            match self.listener.accept().await {
+                Ok((socket, _addr)) => {
+                    let socket = Arc::new(Mutex::new(socket));
+                    let mut con = WebSocketConnection {
+                        socket,
+                        on_message_fkt: None,
+                    };
+                    if let Some(on_connection) = self.on_connection_fkt {
+                        on_connection(&mut con);
+                    }
+                    tokio::spawn(async move { con.connect().await });
+                }
+                Err(e) => error!("couldn't get client: {:?}", e),
+            };
+        }
     }
 }
